7cd2c677424f5c01d25f50c7d24f0f255ff628d3
Ashton <amjsdev@gmail.com>
2023-10-17 22:45:08 -0400

    updated tests

--- example.test.ts
+++ example.test.ts
@@ -3,8 +3,19 @@ import FiniteStateMachine from './fsm'; // Adjust the import path based on your 
 describe('FiniteStateMachine', () => {
   let fsm;
   const states = {
-    start: { EVENT1: 'next' },
-    next: { EVENT2: 'end' },
+    start: {
+      EVENT1: 'next',
+      guards: {
+        EVENT1: () => true, // Allow the transition
+        EVENT2: () => false, // Block the transition
+      },
+    },
+    next: {
+      EVENT2: 'end',
+      guards: {
+        EVENT2: () => true, // Allow the transition
+      },
+    },
     end: {},
   };
 
@@ -17,10 +28,17 @@ describe('FiniteStateMachine', () => {
     expect(fsm.currentState).toBe('start');
   });
 
-  it('should transition to the next state', () => {
+  it('should transition to the next state with a true guard', () => {
     fsm.start('start');
     fsm.transition('EVENT1');
     expect(fsm.currentState).toBe('next');
+  });
+
+  it('should handle transitions with a false guard', () => {
+    fsm.start('start');
+    fsm.transition('EVENT2');
+    expect(fsm.currentState).toBe('start'); // Guard should block the transition
+    // Ensure the error message is logged or handled appropriately in your code.
   });
 
   it('should handle transitions between states', () => {
