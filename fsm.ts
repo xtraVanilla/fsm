b24f1caef45efce192289f77abe7afbfdec7dcca
Ashton <amjsdev@gmail.com>
2023-10-17 22:41:27 -0400

    transition guards

--- fsm.ts
+++ fsm.ts
@@ -15,19 +15,40 @@ class FiniteStateMachine {
     }
   }
 
-  transition(event) {
+  canTransition(event): boolean {
     if (this.currentState) {
-      const nextState = this.states[this.currentState][event];
+      const currentStateConfig = this.states[this.currentState];
 
-      if (nextState) {
-        this.currentState = nextState;
+      if (currentStateConfig && currentStateConfig.guards) {
+        const transitionGuard = currentStateConfig.guards[event];
+
+        if (transitionGuard) {
+          return transitionGuard();
+        }
+      }
+      return true;
+    }
+
+    return false;
+  }
+
+  transition(event) {
+    if (this.canTransition(event)) {
+      if (this.currentState) {
+        const nextState = this.states[this.currentState][event];
+
+        if (nextState) {
+          this.currentState = nextState;
+        } else {
+          console.error(
+            `Transition '${event}' is not defined in the current state.`
+          );
+        }
       } else {
-        console.error(
-          `Transition '${event}' is not defined in the current state.`
-        );
+        console.error(`No state is set. Use start to set an initial state.`);
       }
     } else {
-      console.error(`No state is set. Use start to set an initial state.`);
+      console.error(`Guard prevents transition on event '${event}'.`);
     }
   }
 }
